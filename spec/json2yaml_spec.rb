feaea106cc1954e6da1f256de2cb7013a224d72c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-11-07 13:39:36 +0000

    Fix to process as stream

--- spec/json2yaml_spec.rb
+++ spec/json2yaml_spec.rb
@@ -1,13 +1,31 @@
 require 'spec_helper'
 
 describe Json2yaml do
-  let(:json_str) { "{\"a\":\"b\"}" }
-  let(:yaml_str) { "---\na: b\n" }
-  context '.to_json' do
-    it { expect(Json2yaml.to_yaml(json_str)).to eq(yaml_str) }
+  context "with single data" do
+    let(:json_str) { "{\"a\":\"b\"}\n" }
+    let(:yaml_str) { "---\na: b\n" }
+    let(:json_io)  { StringIO.new(json_str, "r") }
+    let(:yaml_io)  { StringIO.new(yaml_str, "r") }
+    context '.to_json' do
+      it { expect(Json2yaml.to_yaml(json_io).reduce(&:+)).to eq(yaml_str) }
+    end
+
+    context '.to_yaml' do
+      it { expect(Json2yaml.to_json(yaml_io).reduce(&:+)).to eq(json_str) }
+    end
   end
 
-  context '.to_yaml' do
-    it { expect(Json2yaml.to_json(yaml_str)).to eq(json_str) }
+  context "with multiple data" do
+    let(:json_str) { "{\"a\":\"b\"}\n{\"c\":\"d\"}\n{\"e\":\"f\"}\n" }
+    let(:yaml_str) { "---\na: b\n---\nc: d\n---\ne: f\n" }
+    let(:json_io)  { StringIO.new(json_str, "r") }
+    let(:yaml_io)  { StringIO.new(yaml_str, "r") }
+    context '.to_json' do
+      it { expect(Json2yaml.to_yaml(json_io).reduce(&:+)).to eq(yaml_str) }
+    end
+
+    context '.to_yaml' do
+      it { expect(Json2yaml.to_json(yaml_io).reduce(&:+)).to eq(json_str) }
+    end
   end
 end
