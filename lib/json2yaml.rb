0bbd51f52e5954132bbaca484d57a74b9ad5b471
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-11-12 02:03:02 +0900

    Load by arguments

--- lib/json2yaml.rb
+++ lib/json2yaml.rb
@@ -3,18 +3,26 @@ require 'yajl/json_gem'
 require 'yaml'
 
 module Json2yaml
-  def self.to_yaml(json_stream)
+  def self.to_yaml(json_stream, argv)
+    inputs = argv.empty? ? [json_stream] : argv.map{|file| open(file) }
+
     Enumerator.new do |yielder|
-      Yajl::Parser.new.parse(json_stream) do |d|
-        yielder.yield d.to_yaml
+      inputs.each do |input|
+        Yajl::Parser.new.parse(input) do |d|
+          yielder.yield d.to_yaml
+        end
       end
     end
   end
 
-  def self.to_json(yaml_stream)
+  def self.to_json(yaml_stream, argv)
+    inputs = argv.empty? ? [yaml_stream] : argv.map{|file| open(file) }
+
     Enumerator.new do |yielder|
-      YAML.load_stream(yaml_stream) do |d|
-        yielder.yield d.to_json + "\n"
+      inputs.each do |input|
+        YAML.load_stream(input) do |d|
+          yielder.yield d.to_json + "\n"
+        end
       end
     end
   end
